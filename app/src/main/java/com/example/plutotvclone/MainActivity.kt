d7358f1ce65076c0fa90b1c5ab6d814a128e1299
SergioSoto19 <ser192000gio@gmail.com>
2024-08-26 15:15:52 -0500

    seccion de canales fondo y formato adecuado

--- app/src/main/java/com/example/plutotvclone/MainActivity.kt
+++ app/src/main/java/com/example/plutotvclone/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.plutotvclone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val buttonNames = listOf("Pluto TV", "Películas", "Series", "Retro", "Novelas", "Reality", "Competencia")
+        val menuLinearLayout = findViewById<LinearLayout>(R.id.menuFil)
+
+        for (name in buttonNames) {
+            val button = Button(this)
+            button.text = name
+            button.setTextAppearance(R.style.MenuButtonStyle1)
+            menuLinearLayout.addView(button)
+        }
+
+        val highlightedItems = listOf("Canal 1", "Canal 2")
+        val movieItems = listOf("Acción", "Drama", "Comedia","suspenso","Terror","Aventura","Animación")
+
+        val highlightedSection = findViewById<LinearLayout>(R.id.highlightedSection)
+        val moviesSection = findViewById<LinearLayout>(R.id.moviesSection)
+
+        for (item in highlightedItems) {
+            val view = layoutInflater.inflate(R.layout.item_layout, highlightedSection, false)
+            val textView = view.findViewById<TextView>(R.id.textView)
+            textView.text = item
+            highlightedSection.addView(view)
+        }
+
+        for (item in movieItems) {
+            val view = layoutInflater.inflate(R.layout.item_layout, moviesSection, false)
+            val textView = view.findViewById<TextView>(R.id.textView)
+            textView.text = item
+            moviesSection.addView(view)
+        }
+
+
+
+
     }
 }
